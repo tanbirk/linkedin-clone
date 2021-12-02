8785fbebd7c251439276e68057ff7193218e8b3b
tanbirk <tanvir.kashyap879@gmail.com>
2021-12-02 15:10:23 +0530

    fsf

---
+++ src/App.js
@@ -0,0 +1,19 @@
+ import React from 'react';
+import './App.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="">
+
+    {/* HEADER */}
+      <Header />
+    {/* APP BODY */}
+        {/* SIDEBAR */}
+        {/* FEED
+        WIDGETS */}
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Header.js
@@ -0,0 +1,29 @@
+import React from "react"
+import './Header.css'
+
+
+function Header() {
+    return (
+        <div className="header">
+            <h1>this is the header</h1>    
+
+            <div className="header__left">
+                    <img src="" alt="" />
+                    <div className="header__search">
+                        {/* SearchIcon */}
+                        <input type="text"/>
+                    </div>
+            </div>
+
+            <div className="header__right">
+
+
+            </div>
+
+
+        </div>
+    )
+}
+
+export default Header
+
