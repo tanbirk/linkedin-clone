d566dcd903bd6470725c9692851c8d9d3b41bb56
tanbirk <tanvir.kashyap879@gmail.com>
2021-12-08 22:44:26 +0530

    crazy stuff

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
  
-import React from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
-import { selectUser } from './features/userSlice';
+import { login, logout, selectUser } from './features/userSlice';
 import Feed from './Feed';
 import Header from './Header';
 import Sidebar from './Sidebar'
 import Login from './Login';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
+import { auth } from './firebase';
 
 
 function App() {
   const user = useSelector(selectUser)
+  const dispatch = useDispatch()
 
-
+  useEffect(() => {
+    auth.onAuthStateChanged(userAuth => {
+      if (userAuth) {
+          dispatch(login({
+            email: userAuth.email,
+            uid: userAuth.uid,
+            displayName: userAuth.photoURL,
+          }))
+      } else {
+          dispatch(logout());
+      }
+    })
+  },[])
 
   return (
     <div className="app">
